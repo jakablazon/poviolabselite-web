563eb711a84150424cc737db1aac67021b56f364
Marko KR <orcmusic@gmail.com>
2017-03-17 00:50:16 +0100

    fixed join to the circle scrool, fixed founders tabs, added title

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,26 @@
-$(document).ready(function() {
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
+$(document).ready(function() {
+	$('.member').on('click mouseover', function () {
+		var tab_id = $(this).attr('data-tab');
+
+		$('.member').removeClass('current');
+		$('.tabs p').removeClass('current');
+
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	});
 });
